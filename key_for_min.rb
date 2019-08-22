4dee5f101bd7d31852da8aa0209d14e400ff467f
Phyllis Kan <pyk9q7@gmail.com>
2019-08-22 14:04:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
 
   name_hash.each do |key, value|
     if smallest_value == 0 || value < smallest_value
-      smallest_value = value 
+      smallest_value = value
       smallest_key = key
     end
   end
